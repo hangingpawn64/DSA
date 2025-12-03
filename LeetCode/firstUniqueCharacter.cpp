f716211599474bbbb1c6044d9a5917323071772f
hangingpawn64 <2024bcs156@sggs.ac.in>
2025-12-04 01:37:08 +0530

    387. First Unique Character in a String

---
+++ LeetCode/firstUniqueCharacter.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Solution
+    {
+    public:
+        int firstUniqChar(string s)
+        {
+            vector<int> freq(26.0);
+            for (char c : s)
+            {
+                freq[c - 'a']++;
+            }
+
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (freq[s[i] - 'a'] == 1)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    };
+    return 0;
+}
