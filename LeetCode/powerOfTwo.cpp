ad0798a5468eed6e1be285d9849ae7070570f6ab
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-02-24 02:59:24 +0530

    231. Power Of Two

---
+++ LeetCode/powerOfTwo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Solution
+    {
+    public:
+        bool isPowerOfTwo(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+            return !(n & n - 1);
+        }
+    };
+    return 0;
+}
