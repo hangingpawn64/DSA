0a611b9a7a0be15e0e4e3d9b1970334c01ecdfcb
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-02-24 02:45:03 +0530

    1025. Divisor Game

---
+++ LeetCode/divisonGame.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Solution
+    {
+    public:
+        bool divisorGame(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    };
+    return 0;
+}
---
+++ LeetCode/longestCommonPrefix.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<string> s = {"Hello", "Hii", "akshit"};
+    cout<<s[0][1];
+    return 0;
+}
