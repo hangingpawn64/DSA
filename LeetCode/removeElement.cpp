ccbc0e6b803ad2c5ac866f4396a58a15dda1023f
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-02-24 17:08:03 +0530

    27. Remove Element

---
+++ LeetCode/removeElement.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Solution
+    {
+    public:
+        int removeElement(vector<int> &nums, int val)
+        {
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] == val)
+                {
+                    nums.erase(nums.begin() + i);
+                    i--;
+                }
+            }
+            return nums.size();
+        }
+    };
+    return 0;
+}
