ebf66de2ed8bc05f20d929f98e522cdee73d187f
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-02-24 16:51:18 +0530

    remove duplicates from sorted array

---
+++ LeetCode/removeDuplicates.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Solution
+    {
+    public:
+        int removeDuplicates(vector<int> &nums)
+        {
+            for (int i = 1; i < nums.size(); i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    nums.erase(nums.begin() + i);
+                    i--;
+                }
+            }
+            return nums.size();
+        }
+    };
+    return 0;
+}
