0adb63c7f6d2b206e597892ca3482738abea29a2
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-04-04 03:02:15 +0530

    optimal approach

--- LeetCode/aBeforeB.cpp
+++ LeetCode/aBeforeB.cpp
@@ -6,30 +6,37 @@ int main()
     class Solution
     {
     public:
-        bool checkString(string s)
-        {
-            int bStart = -1;
-            for (int i = 0; i < s.size(); i++)
-            {
-                if (s[i] == 'b')
-                {
-                    bStart = i;
-                    break;
-                }
-            }
-            if (bStart == -1)
-            {
-                return true;
-            }
-            for (int i = bStart; i < s.size(); i++)
-            {
-                if (s[i] == 'a')
-                {
-                    return false;
-                }
-            }
-            return true;
+        // bool checkString(string s)
+        // {
+        //     int bStart = -1;
+        //     for (int i = 0; i < s.size(); i++)
+        //     {
+        //         if (s[i] == 'b')
+        //         {
+        //             bStart = i;
+        //             break;
+        //         }
+        //     }
+        //     if (bStart == -1)
+        //     {
+        //         return true;
+        //     }
+        //     for (int i = bStart; i < s.size(); i++)
+        //     {
+        //         if (s[i] == 'a')
+        //         {
+        //             return false;
+        //         }
+        //     }
+        //     return true;
+        // }
+        // Optimal Soln
+        bool checkString(string s) {
+        for(int i=0; i<s.size()-1;i++){
+            if(s[i] == 'b' && s[i+1]=='a'){return false;}
         }
+        return true;
+    }
     };
     return 0;
 }
