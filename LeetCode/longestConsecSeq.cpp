189e919feb606cf29d3ef0627da662587f1555c9
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-04-13 23:55:25 +0530

    128. Longest Consecutive Sequence

---
+++ LeetCode/longestConsecSeq.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    class Solution
+    {
+    public:
+        int longestConsecutive(vector<int> &nums)
+        {
+            if (nums.size() == 0)
+            {
+                return 0;
+            }
+            int longest = 1;
+            unordered_set<int> st;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                st.insert(nums[i]);
+            }
+
+            for (auto it : st)
+            {
+                if (st.find(it - 1) == st.end())
+                { // this means it was not found
+                    int cnt = 1;
+                    int x = it;
+                    while (st.find(x + 1) != st.end())
+                    {
+                        x = x + 1;
+                        cnt = cnt + 1;
+                    }
+                    longest = max(longest, cnt);
+                }
+            }
+            return longest;
+        }
+    };
+    return 0;
+}
