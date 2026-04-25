762a46bb1abbcc572d231751cab9179362afdda3
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-04-25 22:32:44 +0530

    2sum optimal

--- LeetCode/twoSum.cpp
+++ LeetCode/twoSum.cpp
@@ -3,26 +3,51 @@ using namespace std;
 
 int main()
 {
+    // Brute
+    //  class Solution
+    //  {
+    //  public:
+    //      vector<int> twoSum(vector<int> &nums, int target)
+    //      {
+    //          vector<int>::iterator it1 = nums.begin();
+    //          vector<int>::iterator it2 = nums.begin();
+    //          vector<int> result;
+    //          for (int i = 0; i < nums.size(); i++)
+    //          {
+    //              for (int j = i; j < nums.size(); j++)
+    //              {
+    //                  if (nums[i] + nums[j] == target && i != j)
+    //                  {
+    //                      result.push_back(i);
+    //                      result.push_back(j);
+    //                  }
+    //              }
+    //          }
+    //          return result;
+    //      }
+    //  };
+
+    // Optimal
     class Solution
     {
     public:
         vector<int> twoSum(vector<int> &nums, int target)
         {
-            vector<int>::iterator it1 = nums.begin();
-            vector<int>::iterator it2 = nums.begin();
-            vector<int> result;
+            unordered_map<int, int> mpp;
+            vector<int> res;
             for (int i = 0; i < nums.size(); i++)
             {
-                for (int j = i; j < nums.size(); j++)
+                int a = nums[i];
+                int req = target - a;
+                if (mpp.find(req) != mpp.end())
                 {
-                    if (nums[i] + nums[j] == target && i != j)
-                    {
-                        result.push_back(i);
-                        result.push_back(j);
-                    }
+                    res.push_back(i);
+                    res.push_back(mpp[req]);
+                    break;
                 }
+                mpp[a] = i;
             }
-            return result;
+            return res;
         }
     };
     return 0;
