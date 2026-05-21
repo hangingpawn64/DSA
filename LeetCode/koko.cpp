0a6684516031f977ef6fb78cd42545bf85059b16
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-05-21 23:39:52 +0530

    875. Koko Eating Bananas

--- LeetCode/koko.cpp
+++ LeetCode/koko.cpp
@@ -1,15 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int total = accumulate(piles.begin(),piles.end(),0);
-        float hours = total/h;
-        cout<<"total = "<<total<<" "<<"hours = "<<hours<<endl;
-        return ceil(hours);
+int main(){
+    class Solution {
+    public:
+
+    bool isPos(vector<int>& piles, int mid, int h){
+        double  hReq = 0;
+        for(int i=0; i<piles.size(); i++){
+            hReq += ceil((double)piles[i]/mid);
+        }
+        if(hReq > h){return false;}
+        return true;
     }
 
-int main(){
-    vector<int>piles = {3,6,7,11};
-    cout<<minEatingSpeed(piles, 8);
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = INT_MIN;
+
+        //finding max pile
+        for(int i=0; i<piles.size(); i++){
+            if(piles[i]>high){high = piles[i];}
+        }
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(isPos(piles,mid,h)){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
     return 0;
 }
