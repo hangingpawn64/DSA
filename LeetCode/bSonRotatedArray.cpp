f4bb0b11eee7ed6070559f108aede664700cd3c6
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-05-22 18:26:04 +0530

    33. Search in Rotated Sorted Array

--- LeetCode/bSonRotatedArray.cpp
+++ LeetCode/bSonRotatedArray.cpp
@@ -1,49 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    int bS(vector<int>& nums, int start, int end, int target){
-        int low = start, high = end;
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(nums[mid] == target){return mid;}
-            else if(nums[mid]>target){high = mid-1;}
-            else{low = mid+1;}
-        } 
-        return -1;
-    }
+int main(){
+    class Solution {
+    public:
 
     int search(vector<int>& nums, int target) {
         int n = nums.size();
+        int low = 0;
+        int high = n-1;
 
-        //getting smallest number
-        int low = 0, high = n-1;
-        while(low<high){
-            int mid = (low+high)/2;
-            if(nums[mid]>nums[high]){
-                low = mid+1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){return mid;}
+
+            if(nums[low] <= nums[mid]) /* left half is sorted*/{
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
-            else{
-                high = mid;
+            else /* right half is sorted*/{
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
-
-        int smallestIndex = low;
-
-        if(target >= nums[smallestIndex] && target <= nums[n-1]){
-            // BS on 0-> SMALLEST-1
-            return bS(nums, smallestIndex, n-1, target);
-        }
-
-        else{
-            // BE ON Smallest -> Last
-            return bS(nums, 0, smallestIndex-1,target);
-        }
-        return 0;
+        return -1;
     }
-
-
-int main(){
-    vector<int> nums = {3,1};
-    cout<<search(nums, 0);
+};
     return 0;
 }
