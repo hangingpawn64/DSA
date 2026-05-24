e31b345d1840d3441acd4a5c0c4441e3a7f4171c
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-05-24 19:11:48 +0530

    94. Binary Tree Inorder Traversal

---
+++ Trees/inorder.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>in;
+        stack<pair<TreeNode*, int>>st;
+        st.push({root, 1});
+
+        if(root == nullptr){return in;}
+
+        while(!st.empty()){
+            auto it = st.top();
+            st.pop();
+
+            if(it.second == 1){
+                it.second++;
+                st.push(it);
+
+                if(it.first->left != nullptr){
+                    st.push({it.first->left , 1});
+                }
+            }
+
+            else if(it.second == 2){
+                in.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+
+                if(it.first->right != nullptr){
+                    st.push({it.first->right, 1});
+                }
+            }
+        }
+        return in;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
