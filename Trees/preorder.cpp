4adb34180ccab05c65ecf949b96c95b20334d4c4
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-05-24 18:57:48 +0530

    144. Binary Tree Preorder Traversal

---
+++ Trees/preorder.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+    public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<pair<TreeNode* , int>> st;
+        st.push({root, 1});
+        vector<int>pre;
+
+        if(root == nullptr){return pre;}
+
+        while(!st.empty()){
+            auto it = st.top();
+            st.pop();
+            
+            if(it.second == 1){
+                pre.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+
+                if(it.first->left != nullptr){
+                    st.push({it.first->left , 1});
+                }
+            }
+
+            else if(it.second == 2){
+                it.second++;
+                st.push(it);
+
+                if(it.first->right != nullptr){
+                    st.push({it.first->right , 1});
+                }
+            }
+        }
+        return pre;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
