caee5b0b2a300d15d67f9f78f067e757ebd20a07
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-05-31 01:37:35 +0530

    67. Add Binary

--- LeetCode/addBinary.cpp
+++ LeetCode/addBinary.cpp
@@ -1,33 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    string toBinary(int n){
-        if(n==0){return "0";}
-        else{
-            int len = (int)(log2(n));
-            return bitset<32>(n).to_string().substr(32-len-1); 
-        }
-    }
-
-    int binToDec(string s){
-        int num=0;
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == '1'){
-                num += 1 * (pow(2, s.size() - 1 - i));
+int main(){
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        if(a.size()>b.size()){
+            while(a.size() != b.size()){
+                b.insert(0,1,'0');
             }
         }
-        return num;
+        else if(a.size() < b.size()){
+            while(a.size() != b.size()){
+                a.insert(0,1,'0');
+            }
+        }
+        int A = a.size()-1;
+        int B = b.size()-1;
+        bool carry = 0;
+        string ans;
+        while(A >= 0 || B >= 0){
+            if(((a[A] - '0' == 1) && (b[B] - '0' == 1)) && !carry){
+                ans.insert(0,1, '0');
+                carry = true;
+            }
+            else if(((a[A] - '0' == 1) && (b[B] - '0' == 1)) && carry){
+                ans.insert(0,1, '1');
+                carry = true;
+            }
+            else if(((a[A] - '0' == 0) && (b[B] - '0' == 0)) && carry) {
+                ans.insert(0,1, '1');
+                carry = false;
+            }
+            else if(((a[A] - '0' == 0) && (b[B] - '0' == 0)) && !carry) {
+                ans.insert(0,1, '0');
+                carry = false;
+            }
+            else if((a[A] - '0' ^ b[B] - '0') && carry){
+                ans.insert(0,1, '0');
+                carry = true;     
+            }
+            else if((a[A] - '0' ^ b[B] - '0') && !carry){
+                ans.insert(0,1, '1');     
+                carry = false;         
+            }
+            A--; B--;
+        }
+        if(carry){
+            ans.insert(0,1,'1');
+        }
+        return ans;
     }
-
-    string addBinary(string a, string b) {
-        int numA = binToDec(a);
-        int numB = binToDec(b);
-        int sum = numA+numB;
-        string res = toBinary(sum);
-        return res;
-    }
-
-int main(){
-    cout<<addBinary("0", "0");
-    return 0;
+};
 }
