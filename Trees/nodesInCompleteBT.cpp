1dd1960a6553cebb01051c87680987e6fe8582e9
hangingpawn64 <2024bcs156@sggs.ac.in>
2026-06-01 10:14:38 +0530

    222. Count Complete Tree Nodes

---
+++ Trees/nodesInCompleteBT.cpp
@@ -0,0 +1,41 @@
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    
+    int leftHeight(TreeNode* node){
+        int h = 0;
+        while(node != nullptr){
+            h++;
+            node = node->left;
+        }
+        return h;
+    }
+
+    int rightHeight(TreeNode* node){
+        int h = 0;
+        while(node != nullptr){
+            h++;
+            node = node->right;
+        }
+        return h;
+    }
+
+    int countNodes(TreeNode* root) {
+        if(root == nullptr){return 0;}
+
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+
+        if(lh == rh){ return (1 << lh) - 1; }
+        else return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
